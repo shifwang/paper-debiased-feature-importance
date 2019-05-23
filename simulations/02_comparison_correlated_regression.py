27b25be019f18539c186b354622ec7aa64d1dd2a
yu wang <wang.yu@berkeley.edu>
2019-05-23 06:00:02 +0000

    update

--- simulations/02_comparison_correlated_regression.py
+++ simulations/02_comparison_correlated_regression.py
@@ -5,16 +5,12 @@ import shap
 import pandas as pd
 import scipy as sp
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier as rfc
+from sklearn.ensemble import RandomForestRegressor as rfr
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, LabelBinarizer, MinMaxScaler
 from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, mean_squared_error
 import matplotlib.pyplot as plt
 from treeinterpreter.feature_importance import feature_importance as FI
 from treeinterpreter.feature_importance import MDA
-
-def sigmoid(x):
-    x *= 2
-    return np.exp(x) / (np.exp(x) + np.exp(-x))
 
 def permute(X, noisy_features):
     tmp = X.copy()
@@ -22,10 +18,10 @@ def permute(X, noisy_features):
         if noisy_features[j] == 1:
             tmp[:, j] = np.random.permutation(tmp[:, j])
     return tmp
-
 def f(X, noisy_features):
-    probs = sigmoid(np.mean(X[:, noisy_features == 0], 1))
-    return np.array([np.random.choice([0, 1], 1, p=[1 - prob, prob]) for prob in probs]).flatten()
+    y = np.mean(X[:, noisy_features == 0], 1) 
+    y += np.random.normal(0, np.std(y) ** 2 * 100, y.shape)
+    return y
 
 # # Compare different methods in terms of feature selection using simulated data
 debiased_list, gini_list, shap_list, mda_list = [], [], [], []
@@ -42,9 +38,9 @@ for i in range(40):
     n_features = X_train.shape[1]
     n, m = X_train.shape
     names = np.arange(m)
+    scaler = MinMaxScaler()
     noisy_features = np.ones((n_features, ), dtype=int)
     noisy_features[np.random.choice(range(n_features), 5, replace=False)] = 0
-    scaler = MinMaxScaler()
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
     X_train = permute(X_train, noisy_features)
@@ -52,22 +48,21 @@ for i in range(40):
     y_train = f(X_train, noisy_features)
     y_test = f(X_test, noisy_features)
     
-    rf = rfc(n_estimators=100, max_features = 8, min_samples_leaf=100)
+    rf = rfr(n_estimators=100, max_features = 8)
     rf.fit(X_train, y_train)
     gini_imp = rf.feature_importances_
 
-    y_train_one_hot = OneHotEncoder().fit_transform(y_train[:, np.newaxis]).todense()
-    debiased_fi_mean, debiased_fi_std = FI(rf, X_train, y_train_one_hot, type='oob', normalized=False)
+    debiased_fi_mean, debiased_fi_std = FI(rf, X_train, y_train[:, np.newaxis], type='oob', normalized=False)
     debiased_imp = debiased_fi_mean
 
-    MDA_imp, _ = MDA(rf, X_test, y_test[:, np.newaxis], type='test', n_trials=10)
+    MDA_imp, _ = MDA(rf, X_test, y_test[:, np.newaxis], type='test', n_trials=10, metric = 'mse')
 
     explainer = shap.TreeExplainer(rf)
     samples = np.random.choice(range(X_train.shape[0]), 100)
     shap_values = explainer.shap_values(X_train[samples, :])
     shap.summary_plot(shap_values, X_train[samples, :], plot_type="bar")
 
-    shap_imp = np.mean(abs(shap_values[0]), 0) + np.mean(abs(shap_values[1]), 0)
+    shap_imp = np.mean(abs(shap_values), 0) 
 
     debiased_imp[debiased_imp < 0] = 0
     MDA_imp[MDA_imp < 0] = 0
@@ -77,4 +72,4 @@ for i in range(40):
     shap_list.append(roc_auc_score(noisy_features, - shap_imp))
     mda_list.append(roc_auc_score(noisy_features, - MDA_imp))
 
-np.savez('../intermediate/02_simulation_results_correlated_shallow.npz', debiased_list = debiased_list, gini_list=gini_list, shap_list=shap_list, mda_list=mda_list)
+np.savez('../intermediate/02_simulation_results_correlated_regression.npz', debiased_list = debiased_list, gini_list=gini_list, shap_list=shap_list, mda_list=mda_list)
--- simulations/03_generate_simulated_data_correlated.py
+++ simulations/03_generate_simulated_data_correlated.py
@@ -4,35 +4,28 @@ import scipy as sp
 import numpy as np
 from irf.ensemble import wrf
 from sklearn.ensemble import RandomForestClassifier as rfc
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, LabelBinarizer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, LabelBinarizer, MinMaxScaler
 from sklearn.metrics import accuracy_score, roc_auc_score, f1_score
 import matplotlib.pyplot as plt
 from treeinterpreter.feature_importance import feature_importance as FI
 from treeinterpreter.feature_importance import MDA
 def sigmoid(x):
-    x *= 1
+    x *= 2
     return np.exp(x) / (np.exp(x) + np.exp(-x))
+
 def permute(X, noisy_features):
     tmp = X.copy()
     for j in range(X.shape[1]):
-        if noisy_features[j] == 0:
-            continue
-        tmp[:, j] = np.random.permutation(sp.stats.rankdata(tmp[:, j]))
-        maximum = max(tmp[:, j])
-        tmp[:, j] = tmp[:, j] / maximum - .5
+        if noisy_features[j] == 1:
+            tmp[:, j] = np.random.permutation(tmp[:, j])
     return tmp
-def rankdata(X):
-    tmp = X.copy()
-    for j in range(X.shape[1]):
-        #tmp[:, j] = np.random.permutation(sp.stats.rankdata(tmp[:, j]))
-        maximum = max(tmp[:, j])
-        tmp[:, j] = tmp[:, j] / maximum - .5
-    return tmp
+
 def f(X, noisy_features):
-    probs = sigmoid(np.mean(X[:, noisy_features == 0], 1) )
+    probs = sigmoid(np.mean(X[:, noisy_features == 0], 1))
     return np.array([np.random.choice([0, 1], 1, p=[1 - prob, prob]) for prob in probs]).flatten()
 
 for ind in range(40):
+    
     X_train = np.loadtxt('../intermediate/02_enhancer/X_train.csv', delimiter=',')
     y_train = np.loadtxt('../intermediate/02_enhancer/y_train.csv', delimiter=',')
     X_test = np.loadtxt('../intermediate/02_enhancer/X_test.csv', delimiter=',')
@@ -45,10 +38,13 @@ for ind in range(40):
     names = np.arange(m)
     noisy_features = np.ones((n_features, ), dtype=int)
     noisy_features[np.random.choice(range(n_features), 5, replace=False)] = 0
-    y_train = f(X_train, noisy_features)
-    y_test = f(X_test, noisy_features)
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
     X_train = permute(X_train, noisy_features)
     X_test = permute(X_test, noisy_features)
+    y_train = f(X_train, noisy_features)
+    y_test = f(X_test, noisy_features)
 
     np.savetxt('../intermediate/02_enhancer/permuted{}_X_train_correlated.csv'.format(ind), X_train, delimiter=',')
     np.savetxt('../intermediate/02_enhancer/permuted{}_y_train_correlated.csv'.format(ind), y_train, delimiter=',')
